aa6f9e7a64e294a9d22e5ef83b668a75c36b284d
fazel <mohammad.fazel.zare20@gmail.com>
2025-03-19 20:30:32 +0330

    v2 calculator app

---
+++ src/components/OrginallPage.jsx
@@ -0,0 +1,13 @@
+import Btn from "./BtnCalculator"
+import { Fragment } from "react"
+
+export default function OrginallPagte() {
+
+    return (
+        <Fragment>
+            <div>
+                <Btn />
+            </div>
+        </Fragment>
+    )
+}
