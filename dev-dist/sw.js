3e2de211b56ae1627c34870aef2a7c92410e7679
fazel <mohammad.fazel.zare20@gmail.com>
2025-03-19 20:47:09 +0330

    sw add

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ndpdt8pcrp"
+    "revision": "0.du9pm8pr9v8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
